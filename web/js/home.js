3ea75b595e0f3bbacba72d139e6b7bbc414c0b09
Steve King <steve@mydev.co>
2015-01-10 10:16:22 +0000

    Add some content for goals

---
+++ web/js/home.js
@@ -0,0 +1,12 @@
+jQuery(function () {
+
+    jQuery('.carousel')
+        .on('carouselComplete', function () {
+            debugger;
+        })
+        .on('click', 'a', function (e) {
+            //e.preventDefault();
+
+        });
+
+});
